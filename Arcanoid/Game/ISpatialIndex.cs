e81bf81c8a9f481e1a7f7510ca286b2c05bea7d7
kavaler <littlesonofsun@gmail.com>
2025-12-21 22:22:10 +0700

    Доделали game

---
+++ Arcanoid/Game/ISpatialIndex.cs
@@ -0,0 +1,12 @@
+﻿using Arkanoid.Core.Entities;
+using System.Collections.Generic;
+
+namespace Arkanoid.Core.Game
+{
+    public interface ISpatialIndex
+    {
+        void AddBlock(Block block);
+        IEnumerable<Block> GetNearbyBlocks(Vector2 position, float radius);
+        void Clear();
+    }
+}
